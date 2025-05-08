4ce3ca6d386f8fe4d2c2e4e9d8a07403e6d014a7
Hussein-Eissa <Hussein.ShabanIS21@fcai.usc.edu.eg>
2025-05-08 15:59:56 +0300

    Handle responsive tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// import Routes from './appRouter.jsx';
-// import { RouterProvider } from 'react-router-dom';
-// import { ModalProvider } from "./Context/CategoryContext.jsx";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
-// import "@fortawesome/fontawesome-free/css/all.min.css";
-// import { ToastContainer } from 'react-toastify';
-
-
-
-
-// function App() {
-//   return (
-//     <>
-//       <ToastContainer />
-//       <ModalProvider>
-//         <RouterProvider router={Routes} />
-//       </ModalProvider>
-//     </>
-
-//   )
-// }
-
-// export default App
-
-
 import Routes from './appRouter.jsx';
 import { RouterProvider } from 'react-router-dom';
 import { ModalProvider } from "./Context/CategoryContext.jsx";
@@ -44,7 +18,6 @@ function App() {
 
 export default App;
 
-// فصلنا الجزء دا في كومبوننت علشان ناخد القيمة من الكونتكست
 import React, { useContext } from "react";
 import { ThemeContext } from './Context/ThemeContext';
 
--- src/Components/Home/TaskList.jsx
+++ src/Components/Home/TaskList.jsx
@@ -5,13 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const TaskList = ({ tasks, loading, handleEdit, handleDelete, handleView }) => {
   return (
-    <div className="container my-4">
+    <div className="container-fluid m-auto my-4">
       {loading ? (
         <p style={{ fontFamily: "Cairo", fontSize: 20 }}>Loading tasks...</p>
       ) : tasks.length === 0 ? (
         <p style={{ fontFamily: "Cairo", fontSize: 20 }}>No tasks available.</p>
       ) : (
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-1 row-cols-lg-3 g-3">
+        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
           {tasks.map((task) => (
             <div key={task.id || `${task.name}-${task.category}-${task.date}`} className="col">
               <TaskItem
